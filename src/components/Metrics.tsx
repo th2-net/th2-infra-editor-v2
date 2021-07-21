a4ec88af0a35b965cb223fc56c6e9c7b9e36ef5e
Artem Davidovich <artem.davidovich@exactprosystems.com>
2021-07-21 19:35:46 +0400

    Add grafana metriks.

--- src/components/Metrics.tsx
+++ src/components/Metrics.tsx
@@ -1,5 +1,5 @@
 /** *****************************************************************************
- * Copyright 2020-2020 Exactpro (Exactpro Systems Limited)
+ * Copyright 2020-2021 Exactpro (Exactpro Systems Limited)
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -14,24 +14,58 @@
  * limitations under the License.
  ***************************************************************************** */
 
+import { reaction } from 'mobx';
+import { useEffect, useMemo, useState } from 'react';
 import { createUseStyles } from 'react-jss';
+import { useBoxesStore } from '../hooks/useBoxesStore';
+import { useSchemaStore } from '../hooks/useSchemaStore';
+import { BoxEntity } from "../models/Box";
 
 const useStyles = createUseStyles({
 	container: {
 		border: '1px solid',
 		gridArea: 'metrics',
 		borderRadius: 6,
+		overflow: 'hidden'
 	},
+	metriks: {
+		height: '100%',
+		width: '100%',
+		border: 'none',
+	}
 });
 
 function Metrics() {
 	const classes = useStyles();
+	const schemaStore = useSchemaStore();
+	const boxesStore = useBoxesStore();
 
-	return (
+	const [componet, setComponet] = useState<string>('');
+
+	const source = useMemo(() => {
+		const namespace = schemaStore.selectedSchema;
+
+		return `http://th2-qa:30000/grafana/d-solo/logs/logs?orgId=1&refresh=10s&var-namespace=th2-${namespace}&var-component=${componet}&theme=light&panelId=8`;
+	}, [schemaStore.selectedSchema, componet]);
+
+
+	useEffect(() => {
+		const boxSubscription = reaction(
+			() => boxesStore.selectedBox,
+			box => {
+				setComponet(box?.name || '')
+			},
+		);
+
+		return boxSubscription;
+	}, [boxesStore.selectedBox]);
+
+	return boxesStore.selectedBox && (
 		<div className={classes.container}>
-			<h2>Metrics</h2>
+			<iframe className={classes.metriks} src={source}></iframe>
 		</div>
 	);
 }
 
 export default Metrics;
+
